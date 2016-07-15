fe75cf62cbd86c23d820a0495ba008acaaa66011
golubenkoilia <golubenkoilia@yandex.ru>
2016-07-15 13:06:13 +0300

    1

---
+++ GoIT/Terrasoft/hw1.2/hw1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = ("Hello world");
+            for (int i = 0; i <str.Length; i++){
+                int dec = Convert.ToInt16(str[i]);
+                string hex = String.Format("{0:X}", dec);
+                Console.WriteLine("Char {0}: dec {1} hex {2}", str[i], dec, hex);
+            }
+            Console.ReadKey();
+        }
+    }
+}
