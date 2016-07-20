c2d4041eaf4e138fef3de776fb47ae87c85a72f0
golubenkoilia <golubenkoilia@yandex.ru>
2016-07-20 15:54:36 +0300

    1

---
+++ GoIT/Terrasoft/hw3.1/hw3.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] logins = new string[] { "adm", "moder", "user" };
+            string[] passwords = new string[] { "qwerty", "pass", "1234" };
+            string[] roles = new string[] { "admin", "moderator", "user" };
+
+            Console.WriteLine("Input your login: ");
+            string log = Console.ReadLine();
+
+            Console.WriteLine("Input your password: ");
+            string pass = Console.ReadLine();
+            
+            
+
+            if(log == logins[0] && pass == passwords[0] )
+            {
+                Console.WriteLine("Hello, Admin!");
+                Console.WriteLine("User: {0}. Password: {1}. Role: {2}.", logins[0], passwords[0], roles[0]);
+                Console.WriteLine("User: {0}. Password: {1}. Role: {2}.", logins[1], passwords[1], roles[1]);
+                Console.WriteLine("User: {0}. Password: {1}. Role: {2}.", logins[2], passwords[2], roles[2]);
+            }
+            else if (log == logins[1] && pass == passwords[1])
+            {
+                Console.WriteLine("We have 3 users in our system");
+            }
+            else if(log == logins[2] && pass == passwords[2])
+            {
+                Console.WriteLine("Hello, User! \n");
+                Console.WriteLine("User with role user: {0}", logins[2]);
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+                Console.ReadKey();
+        }
+    }
+}
---
+++ GoIT/Terrasoft/hw4.1/hw4.1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number:");
+            String _str = Console.ReadLine();
+            Double _firstArg = Convert.ToDouble(_str);        
+            for(int i = 0; i < 999; i++)
+            {
+                Console.WriteLine();
+                String _oper = getOperation();
+                Console.WriteLine();
+                Double _secondArg = getAnotherNumber();
+                Console.WriteLine();
+                Double _res = getResult(_firstArg, _secondArg, _oper);
+                Console.WriteLine("your result :{0}", _res);
+                _firstArg = _res;
+            }   
+            Console.ReadKey();
+        }
+
+        static string getOperation()
+        {
+            Console.WriteLine("choose operation:\n 1: '+' \n 2: '-' \n 3: '/' \n 4: '*' \n 5: '%' \n 6: 'pow' \n");
+            String _oper = Console.ReadLine();
+            return _oper;
+        }
+
+        static double getAnotherNumber()
+        {
+            Console.WriteLine("enter a number:");
+            String _str = Console.ReadLine();
+            Double _secondArg  = Convert.ToDouble(_str);
+            return _secondArg;
+        }
+
+        static double getResult(Double _firstArg, Double _secondArg, String _operArg)
+        {
+            Double result = 0;
+            if (_operArg == "1"|| _operArg == "+" )
+            {
+                result = Addition(_firstArg,_secondArg);
+            }
+            else if(_operArg == "2" || _operArg == "-")
+            {
+                result = Subtraction(_firstArg, _secondArg);
+            }
+            else if(_operArg == "3" || _operArg == "/")
+            {
+                result = Division(_firstArg, _secondArg); 
+            }
+            else if(_operArg == "4" || _operArg == "*")
+            {
+                result = Multiplication(_firstArg, _secondArg);
+            }
+            else if (_operArg == "5" || _operArg == "%")
+            {
+                result = Mod(_firstArg, _secondArg);
+            }
+            else if (_operArg == "6" || _operArg == "pow")
+            {
+                result = Pow(_firstArg, _secondArg);
+            }
+            return result;
+        }
+
+        static double Addition(Double _firstArg, Double _secondArg)
+        {
+            Double result = _firstArg + _secondArg;
+
+            return result;
+        }
+
+        static double Subtraction(Double _firstArg, Double _secondArg)
+        {
+            Double result = _firstArg - _secondArg;
+
+            return result;
+        }
+
+        static double Division(Double _firstArg, Double _secondArg)
+        {
+            Double result = _firstArg / _secondArg;
+
+            return result;
+        }
+
+        static double Multiplication(Double _firstArg, Double _secondArg)
+        {
+            Double result = _firstArg * _secondArg;
+
+            return result;
+        }
+
+        static double Mod(Double _firstArg, Double _secondArg)
+        {
+            Double result = _firstArg % _secondArg;
+
+            return result;
+        }
+
+        static double Pow(Double _firstArg, Double _secondArg)
+        {
+            Double result = Math.Pow(_firstArg, _secondArg);
+
+            return result;
+        }
+
+    }
+
+}
